acf58a663935eee9de7280ece9d57845f69aaa61
Misty Nguyen <mistymadonna@gmail.com>
2016-02-23 21:22:55 -0800

    images are displaying without duplicates

--- js/app.js
+++ js/app.js
@@ -2,14 +2,30 @@ function randomBetween(min, max) {
   return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-var images = ['bag.jpg', 'banana.jpg', 'cthulhu.jpg']
+var images = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg']
 
-for(var i = 0; i <3; i++) {
+var max = images.length - 1
+for(var i = 0; i < 3; i++) {
+  var imgsCopy = images
   var imgEl = document.createElement('img')
-  var index = randomBetween(0,2)
-  var imgName = images[index]
+  var randomIndex = randomBetween(0,max)
+  var imgName = imgsCopy[randomIndex]
   imgEl.src = 'img/' + imgName
   document.body.appendChild(imgEl)
+  // remove item with randomIndex from imgsCopy
+  imgsCopy.splice(randomIndex, 1)
+  // make sure next time in the loop we reduce the random maximum by 1.
+  max--
 }
 
+
+
+
+
+
+
+
+
 ////make sure no duplicates
+//keep track of clicks
+//To do this, you'll want a constructor function that creates an object associated with each image, and has properties for the name of the image, its filepath, the number of times it has been shown, and the number of times it has been clicked.
